8f81243ada8c21002f0bd742ae94cdee970d9dee
sevichok <sevichok41294@gmail.com>
2021-12-06 15:28:31 +0300

    with localStorage saving

--- src/App.js
+++ src/App.js
@@ -6,26 +6,33 @@ import Form from "./components/Form";
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
+let listForLocalStorage = JSON.parse(localStorage.getItem("todo-list") || "[]"); // список передаваемый в localStorage
+
 class App extends React.Component {
   state = {
     filterStatus: "all", // deleted, done
     filterValue: "",
-    todoList: [],
+    todoList: listForLocalStorage,
     deletedTodo: [],
   };
-  
+
   handleCreateTodo = (name) => {
     const id = uuidv4();
 
     this.setState({
       todoList: this.state.todoList.concat([{ name, done: false, id: id }]),
     });
+
     console.log(id);
     console.log(this.state.todoList);
+
+    listForLocalStorage.push({ name, id, done: false });
+    localStorage.setItem("todo-list", JSON.stringify(listForLocalStorage));
   };
 
   render() {
     const { todoList } = this.state;
+
 
     return (
       <div className="container">
