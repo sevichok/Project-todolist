67ec5ab09d933c50eae4da61188577438e89e8f8
sevichok <sevichok41294@gmail.com>
2022-01-11 00:27:42 +0300

    form on hooks

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,98 +3,63 @@ import Button from "../Button";
 import Input from "../Input";
 
 import { validateForm } from "./validationHelper/validateForm";
-import React from "react";
+import React, { useState } from "react";
 
 
-class Form extends React.Component {
-    state = {
-        name: " ",
-        error: validateForm(" "),
-        touched: false,
+function Form({ onCreateTodo }) {
+
+    const [name, setName] = useState(' ');
+    const [error, setError] = useState(validateForm(" "));
+    const [touched, setTouched] = useState(false);
+
+    const handleBlur = () => { setTouched(true) };
+
+    const handleChange = (e) => {
+        setName(e.target.value);
+        setError(validateForm(e.target.value));
     };
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value,
-            error: validateForm(e.target.value),
-        });
-    };
-
-    handleBlur = () => {
-        this.setState({
-            touched: true,
-        });
-    };
-
-    handleSubmit = (e) => {
-        // console.log(e);
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        const { onCreateTodo } = this.props;
-        const { name, error, touched } = this.state;
-
         if (error && !touched) {
-            this.setState({ touched: true });
+            setTouched(true);
             return;
         }
 
         if (!error) {
             onCreateTodo(name);
-            this.setState({
-                name: " ",
-                error: validateForm(""),
-                touched: false
-            });
+            setTouched(false);
+            setName(" ");
+            setError(validateForm(""));
         }
     };
 
-    render() {
-        const { name, error, touched, } = this.state;
-
-        return (
-            <div className="formContainer">
-                <Input
-                    name="name"
-                    id="create"
-                    value={name}
-                    label="Новое задание"
-                    error={Boolean((touched && error))}
-                    description={(touched && error)}
-                    placeholder="Название"
-                    onBlur={this.handleBlur}
-                    onChange={this.handleChange}
-                />
-                <div className="divFormButtons">
-                    <Button
-                        onClick={this.handleSubmit}
-                        outlook="contained"
-                        size="large"
-                        type="submit"
-                    >Создать
-                    </Button>
-                </div>
+    return (
+        <div className="formContainer">
+            <Input
+                name="name"
+                id="create"
+                value={name}
+                label="Новое задание"
+                error={Boolean((touched && error))}
+                description={(touched && error)}
+                placeholder="Название"
+                onBlur={handleBlur}
+                onChange={handleChange}
+            />
+            <div className="divFormButtons">
+                <Button
+                    onClick={handleSubmit}
+                    outlook="contained"
+                    size="large"
+                    type="submit"
+                >Создать
+                </Button>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Form;
-
-// const Form = () => {
-//     return (
-//         <div className="formContainer">
-//             <label htmlFor="search"><h4>Новое задание</h4></label>
-//             <Input
-//                 name="name"
-//                 id="create"
-//                 placeholder="Название"
-//                 onChange={this.handleChange}
-//             />
-//             <div className="divFormButtons">
-//                 <Button outlook="contained" size="large" type="button" children='Создать'></Button>
-//             </div>
-//         </div>
-//     )
-// }
-
 
