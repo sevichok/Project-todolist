92dad6c471d100d5c3c64700b191cefab613fc4a
sevichok <sevichok41294@gmail.com>
2022-01-17 16:44:55 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,141 +1,115 @@
-import React from "react";
+import React, { useState, useCallback, useEffect, useMemo } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import Filter from "./components/Filter";
 import List from "./components/List";
 import Form from "./components/Form";
 
-
-
-let listForLocalStorage = JSON.parse(localStorage.getItem("active-list") || "[]"); // список передаваемый в localStorage
+let listForLocalStorage = JSON.parse(localStorage.getItem("active-list") || "[]");
 let listDeletedForLocalStorage = JSON.parse(localStorage.getItem("deleted-list") || "[]");
 
-// const defaultTodo = {
-//   id: uuidv4(),
-//   name: "default todo",
-//   done: false,
-// };
+// const [filterStatus, setFilterStatus] = useState("all");
+// const [filterValue, setFilterValue] = useState("");
+// const [todoList, setTodoList] = useState(listForLocalStorage);
+// const [deletedTodoList, setDeletedTodoList] = useState(listDeletedForLocalStorage);
 
-class App extends React.Component {
-  state = {
-    filterStatus: "all", // deleted, done
-    filterValue: "",
-    todoList: listForLocalStorage,
-    deletedTodoList: listDeletedForLocalStorage,
-  };
+function App() {
 
-  handleCreateTodo = (name) => {
+  const [filterStatus, setFilterStatus] = useState("all");
+  const [filterValue, setFilterValue] = useState("");
+  const [todoList, setTodoList] = useState(listForLocalStorage);
+  const [deletedTodoList, setDeletedTodoList] = useState(listDeletedForLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("active-list", JSON.stringify(todoList));
+    localStorage.setItem("deleted-list", JSON.stringify(deletedTodoList))
+  }, [todoList, deletedTodoList]);
+
+  const handleCreateTodo = (name) => {
     const createId = uuidv4(); //Math.floor(Math.random() * 10000), uuidv4()
+    setTodoList(todoList.concat({
+      name,
+      done: false,
+      id: createId,
+    }));
+  };
 
-    this.setState({
-      todoList: this.state.todoList.concat({
-        name,
-        done: false,
-        id: createId,
-      }),
-    },
-      () => { localStorage.setItem("active-list", JSON.stringify(this.state.todoList)); }
+  const handleDone = (id) => {
+    setTodoList(todoList.map((todoItem) =>
+      todoItem.id === id ? { ...todoItem, done: true } : todoItem
+    ));
+  };
+
+  const handleEdit = (id, name) => {
+    setTodoList(todoList.map((todoItem) => todoItem.id === id ? { ...todoItem, name } : todoItem));
+  };
+
+  const handleDelete = (id) => {
+    const deletedTodoIndex = todoList.findIndex(
+      (todoItem) => todoItem.id === id
     );
+    if (deletedTodoIndex < 0) { return todoList }
+    const deletedTodoItem = (todoList.splice(deletedTodoIndex, 1));
+    setTodoList([...todoList]);
+    setDeletedTodoList(deletedTodoList.concat(deletedTodoItem));
   };
 
-  handleDone = (id) => {
-    console.log(`Pressed button Done on item with ID: ${id}`);
-
-    this.setState((state) => ({
-      todoList: state.todoList.map((todoItem) =>
-        todoItem.id === id ? { ...todoItem, done: true } : todoItem
-      ),
-    }),
-      () => { localStorage.setItem("active-list", JSON.stringify(this.state.todoList)); });
+  const handleFilterChange = (e) => {
+    setFilterValue(e.target.value)
   };
 
-  handleEdit = (id, name) => {
-    console.log(`Pressed button Edit on item with name: ${name}`);
-
-    this.setState((state) => ({
-      todoList: state.todoList.map((todoItem) => todoItem.id === id ? { ...todoItem, name } : todoItem),
-    }),
-    )
-  };
-
-  handleDelete = (id) => {
-    console.log(`Pressed button Delete on item with ID: ${id}`);
-
-    this.setState((state) => {
-      const deletedTodoIndex = state.todoList.findIndex(
-        (todoItem) => todoItem.id === id
-      );
-
-      if (deletedTodoIndex < 0) { return state }
-
-      const deletedTodoItem = state.todoList.splice(deletedTodoIndex, 1);
-
-      return {
-        todoList: [...state.todoList],
-        deletedTodoList: state.deletedTodoList.concat(deletedTodoItem),
-      };
-    },
-      () => {
-        localStorage.setItem("active-list", JSON.stringify(this.state.todoList));
-        localStorage.setItem("deleted-list", JSON.stringify(this.state.deletedTodoList))
-      });
-  };
-
-  getFilteredToDo = () => {
+  const getFilteredToDo = () => {
 
     let todoList = [];
 
-    if (this.state.filterStatus === 'done') {
-      console.log('Pressed filter done')
-      todoList = this.state.todoList.filter((todoItem) => todoItem.done === true)
+    if (filterStatus === 'done') {
+      console.log('Pressed filter done');
+      setFilterStatus('done');
+      setTodoList(todoList.filter((todoItem) => todoItem.done === true))
     }
-    if (this.state.filterStatus === 'all') {
-      console.log('Pressed filter all')
-      todoList = this.state.todoList
+    if (filterStatus === 'all') {
+      console.log('Pressed filter all');
+      setFilterStatus('all');
+      setTodoList(todoList);
     }
-    if (this.state.filterStatus === 'deleted') {
-      console.log('Pressed filter deleted')
-      todoList = this.state.deletedTodoList
+    if (filterStatus === 'deleted') {
+      console.log('Pressed filter deleted');
+      setFilterStatus('deleted');
+      setTodoList(deletedTodoList);
     }
-    if (this.state.filterValue) {
-      todoList = todoList.filter((todoItem) => todoItem.name.toLowerCase().includes(this.state.filterValue.toLowerCase()))
-    }
+    
+    // if (handleFilterChange) {
+    //   setTodoList(todoList.filter((todoItem) => todoItem.name.toLowerCase().includes(filterValue.toLowerCase())))
+    // }
     return todoList
-  }
+  };
 
-  handleFilterChange = (e) => {
-    this.setState({
-      filterValue: e.target.value,
-    });
+  const handleClick = (newFilterStatus) => {
+    setFilterStatus(newFilterStatus)
   };
 
 
-  handleClick = (newFilterStatus) => {
-    this.setState({ filterStatus: newFilterStatus })
-  }
-
-  render() {
-    const todoList = this.getFilteredToDo();
-    console.log(this.getFilteredToDo());
-
-    return (
-      <div className="container">
-        <Header listCount={todoList.length} />
-        <Filter
-          onClick={this.handleClick}
-          inputValue={this.state.filterValue}
-          onFilterChange={this.handleFilterChange} />
-        <List
-          onEdit={this.handleEdit}
-          initialValue={this.state.name}
-          onDone={this.handleDone}
-          list={todoList}
-          hideDeleteTodoBtn={this.state.filterStatus === 'deleted'}
-          onDelete={this.handleDelete} />
-        <Form onCreateTodo={this.handleCreateTodo} />
-      </div>
-    );
-  };
+  return (
+    <div className="container">
+      <Header
+        listCount={getFilteredToDo.length}
+      />
+      <Filter
+        onClick={handleClick}
+        inputValue={filterValue}
+        onFilterChange={handleFilterChange}
+      />
+      <List
+        onEdit={handleEdit}
+        initialValue={todoList.name}
+        onDone={handleDone}
+        list={todoList}
+        hideDeleteTodoBtn={filterStatus === 'deleted'}
+        onDelete={handleDelete}
+      />
+      <Form onCreateTodo={handleCreateTodo} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import './HeaderStyle.css';
+import Button from '../Button';
 
 const Header = ({listCount}) => {
   return (
     <div className="headerContainer">
       <h1 className="title"> Todo list</h1>
-      <h4> Количество записей {listCount}</h4>
+      <h4> Количество записей : {listCount}</h4>
+      <Button outlook="contained">Изменить язык</Button>
+      <Button outlook="contained">Изменить тему</Button>
     </div>
   );
 };
